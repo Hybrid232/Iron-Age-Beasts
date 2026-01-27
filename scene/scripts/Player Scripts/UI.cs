620d9dfdc54bf7f64eaf495a094f55c5ce4259a7
Emmitt Stevenson <47986766+Hybrid232@users.noreply.github.com>
2026-01-27 08:08:16 -0700

    Update UI.cs

--- scene/scripts/Player Scripts/UI.cs
+++ scene/scripts/Player Scripts/UI.cs
@@ -3,66 +3,66 @@ using System.Collections.Generic;
 
 public partial class UI : CanvasLayer
 {
-    [Export] public Texture2D RedHeartTexture;
-    [Export] public Texture2D BlackHeartTexture;
-    [Export] public HBoxContainer HealthContainer;
+	[Export] public Texture2D RedHeartTexture;
+	[Export] public Texture2D BlackHeartTexture;
+	[Export] public HBoxContainer HealthContainer;
 
-    [Export] public Texture2D StaminaFullTexture;
-    [Export] public Texture2D StaminaEmptyTexture;
-    [Export] public HBoxContainer StaminaContainer;
+	[Export] public Texture2D StaminaFullTexture;
+	[Export] public Texture2D StaminaEmptyTexture;
+	[Export] public HBoxContainer StaminaContainer;
 
-    private List<TextureRect> _hearts = new List<TextureRect>();
-    private List<TextureRect> _staminaIcons = new List<TextureRect>();
+	private List<TextureRect> _hearts = new List<TextureRect>();
+	private List<TextureRect> _staminaIcons = new List<TextureRect>();
 
-    public override void _Ready()
-    {
-        // Find all TextureRects inside the container and add them to our list
-        foreach (Node child in HealthContainer.GetChildren())
-        {
-            if (child is TextureRect rect)
-            {
-                _hearts.Add(rect);
-            }
-        }
-        // Initialize display to full health
-        UpdateHealthDisplay(10);
-        
-        foreach (Node child in StaminaContainer.GetChildren())
-        {
-            if (child is TextureRect rect)
-            {
-                _staminaIcons.Add(rect);
-            }
-        }
-        // Initialize Stamina to 5
-        UpdateStaminaDisplay(5);
-    }
+	public override void _Ready()
+	{
+		// Find all TextureRects inside the container and add them to our list
+		foreach (Node child in HealthContainer.GetChildren())
+		{
+			if (child is TextureRect rect)
+			{
+				_hearts.Add(rect);
+			}
+		}
+		// Initialize display to full health
+		UpdateHealthDisplay(10);
+		
+		foreach (Node child in StaminaContainer.GetChildren())
+		{
+			if (child is TextureRect rect)
+			{
+				_staminaIcons.Add(rect);
+			}
+		}
+		// Initialize Stamina to 5
+		UpdateStaminaDisplay(5);
+	}
 
-    public void UpdateHealthDisplay(int currentHealth)
-    {
-        // Loop through all hearts
-        for (int i = 0; i < _hearts.Count; i++)
-        {
-            // If the index is less than current health, it's Red (Alive)
-            // Otherwise, it's Black (Damaged)
-            if (i < currentHealth)
-            {
-                _hearts[i].Texture = RedHeartTexture;
-            }
-            else
-            {
-                _hearts[i].Texture = BlackHeartTexture;
-            }
-        }
-    }
-    public void UpdateStaminaDisplay(int currentStamina)
-    {
-        for (int i = 0; i < _staminaIcons.Count; i++)
-        {
-            if (i < currentStamina)
-                _staminaIcons[i].Texture = StaminaFullTexture;
-            else
-                _staminaIcons[i].Texture = StaminaEmptyTexture;
-        }
-    }
+	public void UpdateHealthDisplay(int currentHealth)
+	{
+		// Loop through all hearts
+		for (int i = 0; i < _hearts.Count; i++)
+		{
+			// If the index is less than current health, it's Red (Alive)
+			// Otherwise, it's Black (Damaged)
+			if (i < currentHealth)
+			{
+				_hearts[i].Texture = RedHeartTexture;
+			}
+			else
+			{
+				_hearts[i].Texture = BlackHeartTexture;
+			}
+		}
+	}
+	public void UpdateStaminaDisplay(int currentStamina)
+	{
+		for (int i = 0; i < _staminaIcons.Count; i++)
+		{
+			if (i < currentStamina)
+				_staminaIcons[i].Texture = StaminaFullTexture;
+			else
+				_staminaIcons[i].Texture = StaminaEmptyTexture;
+		}
+	}
 }
