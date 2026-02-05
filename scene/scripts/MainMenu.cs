e565d2731ffd62246a0a814284cf03fe26a07afd
Emmitt Stevenson <47986766+Hybrid232@users.noreply.github.com>
2026-02-05 09:27:19 -0700

    Menu Completed

--- scene/scripts/MainMenu.cs
+++ scene/scripts/MainMenu.cs
@@ -3,25 +3,49 @@ using System;
 
 public partial class MainMenu : Control
 {
-	// Called when the node enters the scene tree for the first time.
+	private Control _menuContainer;
+	private Control _optionsBG;
+
 	public override void _Ready()
 	{
-		Button startButton = GetNode<Button>("StartButton");
+		_menuContainer = GetNode<Control>("MenuContainer");
+		_optionsBG = GetNode<Control>("OptionsBG");
+
+		_optionsBG.Visible = false;
+
+		Button startButton = GetNode<Button>("MenuContainer/StartButton");
 		startButton.Pressed += OnStartPressed;
-		
-		Button quitButton = GetNode<Button>("QuitButton");
+
+		Button settingsButton = GetNode<Button>("MenuContainer/OptionsButton");
+		settingsButton.Pressed += OnSettingsPressed;
+
+		Button quitButton = GetNode<Button>("MenuContainer/QuitButton");
 		quitButton.Pressed += OnQuitPressed;
+		
+		Button backButton = GetNode<Button>("OptionsBG/BackButton");
+		backButton.Pressed += OnBackPressed;
 	}
 
 	private void OnStartPressed()
 	{
 		GetTree().ChangeSceneToFile("res://scene/Sandbox/TestWorld.tscn");
 	}
-	
+
+	private void OnSettingsPressed()
+	{
+		_menuContainer.Visible = false;
+		_optionsBG.Visible = true;
+	}
+
 	private void OnQuitPressed()
 	{
 		GD.Print("Quitting Game...");
 		GetTree().Quit();
-		
+	}
+	
+	private void OnBackPressed()
+	{
+		_menuContainer.Visible = true;
+		_optionsBG.Visible = false;
 	}
 }
